30862ea4673977b3d43c24ff7fd78064bcc6c8f2
Corentinrhr <62744229+Corentinrhr@users.noreply.github.com>
2024-09-19 12:05:33 +0200

    Update CREATE_TABLE.sql

--- FISA Link/CREATE_TABLE.sql
+++ FISA Link/CREATE_TABLE.sql
@@ -1,37 +1,44 @@
+CREATE TABLE fisa_years (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    year INT NOT NULL,
+    nom_promotion VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE students (
-    id INT(11) NOT NULL AUTO_INCREMENT,    
-    first_name VARCHAR(30) NOT NULL,    
-    last_name VARCHAR(150) NOT NULL,  
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(150) NOT NULL,
     email_tsp VARCHAR(254),
-    PRIMARY KEY (id)  
+    fisa_year_id INT,
+    FOREIGN KEY (fisa_year_id) REFERENCES fisa_years(id)
 );
 
 CREATE TABLE users (
-    id INT(11) NOT NULL AUTO_INCREMENT, 
-    password VARCHAR(128) NOT NULL, 
-    email_perso VARCHAR(254), 
-    is_active TINYINT(1) DEFAULT 1, 
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    password VARCHAR(128) NOT NULL,
+    email_perso VARCHAR(254),
+    is_active TINYINT(1) DEFAULT 1,
     is_superuser TINYINT(1) DEFAULT 0,
     date_joined DATETIME DEFAULT CURRENT_TIMESTAMP,
-    student_id INT(11), 
-    PRIMARY KEY (id), 
-    FOREIGN KEY (student_id) REFERENCES students(id) 
+    student_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(id)
 );
 
 CREATE TABLE data_link (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    fisa_year INT NOT NULL,
+    fisa_year_id INT,
     type VARCHAR(100) NOT NULL,
     link TEXT NOT NULL,
     title VARCHAR(255) NOT NULL,
     `desc` TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (fisa_year_id) REFERENCES fisa_years(id)
 );
 
 CREATE TABLE SIF (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_student INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
     bungalow INT,
     pizza VARCHAR(255),
-    FOREIGN KEY (id_student) REFERENCES students(id)
+    FOREIGN KEY (student_id) REFERENCES students(id)
 );
