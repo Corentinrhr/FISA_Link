dbf48a73a5a4bbcc405eb44648e19737f1ed085a
Corentinrhr <corahier@hotmail.fr>
2024-09-19 14:27:22 +0200

    Python py Env

--- FISA Link/run_api.sh
+++ FISA Link/run_api.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-# Installation lib Python
-source ~/.bashrc 
-pip install django
-pip install mysqlclient
-pip install django-cors-headers
-pip install collectstatic
-pip install -r /app/requirements.txt
+# Chemin vers le répertoire du projet
+PROJECT_DIR="/home/corentinrhr/Documents/FISA_Link/FISA Link"
+
+# Nom de l'environnement virtuel
+ENV_NAME="FISA_Link_py_env"
+
+# Aller dans le répertoire du projet
+cd "$PROJECT_DIR" || { echo "Répertoire du projet non trouvé"; exit 1; }
+
+# Vérifier si l'environnement virtuel existe
+if [ ! -d "$ENV_NAME" ]; then
+    echo "Création de l'environnement virtuel..."
+    python3 -m venv "$ENV_NAME"
+fi
+
+# Activer l'environnement virtuel
+source "$ENV_NAME/bin/activate"
+
+# Installer les dépendances
+echo "Installation des dépendances..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
 while true
 do
--- FISA Link/run_web.sh
+++ FISA Link/run_web.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-# Installation lib Python
-source ~/.bashrc 
-pip install django
-pip install mysql-connector-python
-pip install mysqlclient
-pip install django-cors-headers
-pip install collectstatic
-pip install -r /app/requirements.txt
+# Chemin vers le répertoire du projet
+PROJECT_DIR="/home/corentinrhr/Documents/FISA_Link/FISA Link"
 
+# Nom de l'environnement virtuel
+ENV_NAME="FISA_Link_py_env"
+
+# Aller dans le répertoire du projet
+cd "$PROJECT_DIR" || { echo "Répertoire du projet non trouvé"; exit 1; }
+
+# Vérifier si l'environnement virtuel existe
+if [ ! -d "$ENV_NAME" ]; then
+    echo "Création de l'environnement virtuel..."
+    python3 -m venv "$ENV_NAME"
+fi
+
+# Activer l'environnement virtuel
+source "$ENV_NAME/bin/activate"
+
+# Installer les dépendances
+echo "Installation des dépendances..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Boucle pour lancer le serveur web
 while true
 do
     echo "Lancement du serveur web..."
-    python ./manage.py collectstatic --noinput
-    python ./manage.py makemigrations main --settings=app_web_django.settings_web
-    python ./manage.py migrate main --settings=app_web_django.settings_web
-    python ./manage.py runserver 0.0.0.0:8800 --settings=app_web_django.settings_web
+    python manage.py collectstatic --noinput
+    python manage.py makemigrations main --settings=app_web_django.settings_web
+    python manage.py migrate main --settings=app_web_django.settings_web
+    python manage.py runserver 0.0.0.0:8800 --settings=app_web_django.settings_web
     echo "Le serveur web a crashé. Redémarrage dans 30 secondes..."
     sleep 30
 done
